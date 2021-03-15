8f7b7e29c8b7af09d625f42360750c2b60b4c9dd
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-03-14 23:38:00 -0400

    commit tree 7f2c037a3396cec4630e0b244abcad1b84ac4b78

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+    
+  function superbowlWin(record) {
+    let result = record.find(element => element.result === "W")
+    if (result) {
+        return result.year
+    }
+     }
